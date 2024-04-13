f233912d75ddd111d5dc9dc6731a5e7478946219
eze <mollinedoezequiel@gmail.com>
2024-04-13 20:53:03 -0300

    first commit

---
+++ src/test/java/com/grupo2/parteyreparte/ParteYReparteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.parteyreparte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParteYReparteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
