854da5dfefb842c523d6e2b6eac70a8e40b9a869
eze <mollinedoezequiel@gmail.com>
2024-04-15 21:48:18 -0300

    agrego id en userDTO

--- src/main/java/com/grupo2/parteyreparte/mappers/UserMapper.java
+++ src/main/java/com/grupo2/parteyreparte/mappers/UserMapper.java
@@ -9,6 +9,7 @@ public class UserMapper {
 
     public UserDTO mapToUserDTO(User user){
         UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
         userDTO.setName(user.getName());
         userDTO.setEmail(user.getEmail());
         userDTO.setAge(user.getAge());
