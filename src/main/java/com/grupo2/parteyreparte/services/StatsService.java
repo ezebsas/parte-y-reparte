c1b51dcbefc3dcbb21f4d10079f4be6d5e677b82
Federico Herrera <fedega.herrera@gmail.com>
2024-05-20 20:51:09 -0300

    Completo metodo getUniqueUsersCount

--- src/main/java/com/grupo2/parteyreparte/services/StatsService.java
+++ src/main/java/com/grupo2/parteyreparte/services/StatsService.java
@@ -1,10 +1,17 @@
 package com.grupo2.parteyreparte.services;
 
 
+import com.grupo2.parteyreparte.dtos.ProductDTO;
+import com.grupo2.parteyreparte.dtos.UserDTO;
 import com.grupo2.parteyreparte.repositories.ProductRepository;
 import com.grupo2.parteyreparte.repositories.StatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class StatsService {
@@ -27,7 +34,16 @@ public class StatsService {
 
     public Integer getUniqueUsersCount(){
 
-        return statsRepository.uniqueUsers();
+        List<ProductDTO> productDTOList = productService.getAll();
+
+        Set<String> uniqueUserThatInteract = new HashSet<>();
+
+        productDTOList.forEach(productDTO -> {
+            uniqueUserThatInteract.add(productDTO.getOwner().getEmail());
+            uniqueUserThatInteract.addAll(productDTO.getSubscribers().stream().map(UserDTO::getEmail).toList());
+        });
+
+        return uniqueUserThatInteract.size();
     }
 
 
